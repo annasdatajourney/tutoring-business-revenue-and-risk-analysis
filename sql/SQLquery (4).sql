067da4efb17ddda55196e53240a05754a26a2329
Anna <anna_dudzic@outlook.com>
2026-04-04 22:17:53 +0100

    Delete sql/SQLquery (4).sql

--- sql/SQLquery (4).sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    month_name,
-    COUNT(*) AS disrupted_lessons,
-    SUM(final_price) AS lost_revenue
-FROM lessons
-WHERE status IN ('cancelled_by_student', 'no_show')
-GROUP BY month_name
-ORDER BY lost_revenue DESC;
