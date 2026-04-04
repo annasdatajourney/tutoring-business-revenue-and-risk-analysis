87285b8db4a3729e7168cdce2d4049303a16406f
Anna <anna_dudzic@outlook.com>
2026-04-04 22:17:42 +0100

    Delete sql/SQLquery (3).sql

--- sql/SQLquery (3).sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    age_group,
-    status,
-    COUNT(*) AS lesson_count,
-    SUM(final_price) AS expected_revenue
-FROM lessons
-WHERE status IN ('cancelled_by_student', 'no_show')
-GROUP BY age_group, status
-ORDER BY expected_revenue DESC;
